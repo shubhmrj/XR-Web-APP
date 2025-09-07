1e302b8448767c9bcbe2879c95b250a24f573cfc
Shubham Raj <shubmrj@gmail.com>
2025-09-08 00:26:47 +0530

    push~1

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3.10
 
-import sys
-import os
+# This file contains the WSGI configuration required to serve up your
+# web application at http://<your-username>.pythonanywhere.com/
+# It works by setting the variable 'application' to a WSGI handler of some
+# description.
+#
+# The below has been auto-generated for your Flask project
 
-# Add your project directory to sys.path
+import sys
+
+# add your project directory to the sys.path
 project_home = '/home/XRCHEMLAB/XR-Web-APP'
 if project_home not in sys.path:
     sys.path = [project_home] + sys.path
 
-from app import app as application
-
-if __name__ == "__main__":
-    application.run()
+# import flask app but need to call it "application" for WSGI to work
+from app import app as application  # noqa
